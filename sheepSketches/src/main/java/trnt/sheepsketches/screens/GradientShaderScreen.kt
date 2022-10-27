9146eb42698438d1c42e0b8e2c640ebcb55801ba
5e32a505ad92310e881bdce2 <2978958+drinkthestars@users.noreply.github.com>
2022-10-27 00:24:59 -0700

    Add additional gradient

--- sheepSketches/src/main/java/trnt/sheepsketches/screens/GradientShaderScreen.kt
+++ sheepSketches/src/main/java/trnt/sheepsketches/screens/GradientShaderScreen.kt
@@ -85,21 +85,40 @@ internal fun gradientShader() = RuntimeShader(
         uniform float iTime;
         
         vec4 main(in float2 fragCoord) {
-       
-        // Normalized pixel coordinates (from 0 to 1)
-        vec2 uv = fragCoord/iResolution.xy;
+            // Normalized pixel coordinates (from 0 to 1)
+            vec2 uv = fragCoord/iResolution.xy;
     
-        // Time varying pixel color
-        vec3 col = 0.8 + 0.2*cos(iTime*2.0+uv.xxx*2.0+vec3(1,2,4));
+            // Time varying pixel color
+            vec3 col = 0.8 + 0.2*cos(iTime*2.0+uv.xxx*2.0+vec3(1,2,4));
     
-        // Output to screen
-        return vec4(col,1.0);
-       
-    //    float uvY = fragCoord.y / iResolution.y;
-    //    float progress = (uvY + sin((iTime) * 2.0*3.14)) / 3.0;
-    //    vec3 color1 = vec3(46.0/255.0, 148.0/255.0, 250.0/255.0);
-    //    vec3 color2 = vec3(1, 143.0/255.0, 216.0/255.0);
-    //    return vec4(mix(color1, color2, progress), 1);
+            // Output to screen
+            return vec4(col,1.0);
         }
+    """
+)
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+internal fun noodleZoomShader() = RuntimeShader(
+    """
+        uniform float2 iResolution;
+        uniform float iTime;
+
+// Source: @notargs https://twitter.com/notargs/status/1250468645030858753
+float f(vec3 p) {
+    p.z -= iTime * 10.;
+    float a = p.z * .1;
+    p.xy *= mat2(cos(a), sin(a), -sin(a), cos(a));
+    return .1 - length(cos(p.xy) + sin(p.yz));
+}
+
+half4 main(vec2 fragcoord) { 
+    vec3 d = .5 - fragcoord.xy1 / iResolution.y;
+    vec3 p=vec3(0);
+    for (int i = 0; i < 32; i++) {
+      p += f(p) * d;
+    }
+    return ((sin(p) + vec3(2, 5, 12)) / length(p)).xyz1;
+}
+
     """
 )
